28281e3eafc77fae8787cadba035250a90d71b91
Bento <bentoatual@gmail.com>
2026-04-10 21:24:31 -0300

    Seunga versão do app

--- logica.js
+++ logica.js
@@ -1,15 +1,32 @@
-async function buscarClimaname() {
-    const cidade = document.getElementById('cidade').value;
+async function buscarClima() {
+    const cidade = document.getElementById('Cidade').value;
     const url = `https://goweather.xyz/v2/weather/${cidade}`;
 
     const resposta = await fetch(url);
     const dados = await resposta.json();
 
+    console.log(cidade);
+    console.log(dados);
+    console.log(url);
+
+    if (dados.message === "NOT_FOUND") {
+        document.getElementById("resultado").innerHTML = `
+            <p>Essa cidade não existe</p>
+        `;
+        return;
+    }
+
+    // Caso de SUCESSO
     document.getElementById("resultado").innerHTML = `
     <h2>Clima em ${cidade}</h2>
     <p>Temperatua: ${dados.temperature}</p>
     <p>Vento: ${dados.wind}</p>
-    <p>Descrição: ${dados.descripition}</p>
+    <p>Descrição: ${dados.description}</p>
     `;
-
 }
+
+document.getElementById("Cidade").addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        buscarClima();
+    }
+});
